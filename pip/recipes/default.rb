d6bb7b34da021dba3ea9feeb7d34aa728a73c3f4
Charlie Sharpsteen <source@sharpsteen.net>
2010-11-07 20:20:34 -0800

    Added Pip and associated resources
    The pip instalation specification is quite hacky---it relies on
    `easy_install-2.7` being present in the path.
    
    The rest of the files add support so that Pip may be used by Chef as a provider
    of Python packages.  The provider and resource files are taken from Christopher
    Peplin's repository as discussed in Chef issue 973:
    
        http://tickets.opscode.com/browse/CHEF-973
    
    It looks like a native pip provider may show up in the 0.10 release of Chef.
    
    Modifications had to be made in order for the pip provider to be able to inherit
    from the EasyInstall class outside of the Chef install location.  This was
    acheieved with some tricks borrowed from Corey Donohoe's Smeagol repository
    which pulls similar tricks for Homebrew on OS X:
    
        https://github.com/atmos/smeagol

---
+++ pip/recipes/default.rb
@@ -0,0 +1,11 @@
+case node[:platform]
+when 'debian', 'ubuntu'
+  execute 'easy_install-2.7 pip' do
+    not_if "which pip"
+  end
+else
+  puts <<-EOS.undent
+    Cannot guarentee pip will be installed and linked to python 2.7. Cowardly refusing to continue!
+    Coming up with a good workaround is an area for improvement.
+  EOS
+end
