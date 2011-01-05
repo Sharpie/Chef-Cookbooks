aa365c542fbafa8bfa0d3dbcdd9b4b0c5bc38348
Charlie Sharpsteen <source@sharpsteen.net>
2011-01-05 01:38:08 -0800

    Created cookbook for cherokee webserver
    Capable of getting to "Hello, World!" on Ubuntu.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Charlie Sharpsteen"
+maintainer_email "source@sharpsteen.net"
+license          "Apache 2.0"
+description      "Installs/Configures the cherokee web server"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.mdown'))
+version          "0.0.1"
